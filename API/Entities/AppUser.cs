297a30457586069f626ccf00527b66d5378de8a1
Melissa Gil <mgil-foreflight@foreflight.com>
2023-10-11 14:36:54 -0500

    End of section 4

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -5,4 +5,6 @@ public class AppUser
     // [Key] - specifies PK
     public int Id { get; set; }
     public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PasswordSalt { get; set; }
 }
