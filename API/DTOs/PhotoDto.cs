2a791d1b41dd11731f45fb385b3b8cd8665878b4
Melissa Gil <mgil-foreflight@foreflight.com>
2023-10-17 10:21:46 -0500

    End of section 8

---
+++ API/DTOs/PhotoDto.cs
@@ -0,0 +1,12 @@
+﻿namespace API.Dto
+{
+    public class PhotoDto
+    {
+        public int Id { get; set; }
+        public string Url { get; set; }
+        public bool IsMain { get; set; }
+    }
+
+}
+
+
